2b38cca71fa6f4be0607b24d674dea2a5a79775c
leovillaverde <leobajista@gmail.com>
2015-06-14 18:55:25 -0300

    Imprimir cabecera ASM
    En la acción semántica del programa principal. Un chamuyo.

---
+++ TP3/Final.asm
@@ -0,0 +1,5 @@
+include macros2.asm		 ;incluye macros
+include number.asm		 ;incluye el asm para impresion de numeros
+.MODEL LARGE ; tipo del modelo de memoria usado.
+.386
+.STACK 200h ; bytes en el stack
